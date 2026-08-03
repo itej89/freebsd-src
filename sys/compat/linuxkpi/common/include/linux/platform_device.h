341cd915e9b59313182944a54c42aa75cd7a634f
Tej Kiran <kiran.tej@amd.com>
2026-08-03 16:18:06 -0500

    linuxkpi: Add platform_device/platform_driver bridge

--- sys/compat/linuxkpi/common/include/linux/platform_device.h
+++ sys/compat/linuxkpi/common/include/linux/platform_device.h
@@ -2,27 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2020-2022 Bjoern A. Zeeb
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * Copyright (c) 2026 Tej Kiran
  */
 
 #ifndef	_LINUXKPI_LINUX_PLATFORM_DEVICE_H
@@ -30,68 +10,94 @@
 
 #include <linux/kernel.h>
 #include <linux/device.h>
+#include <linux/mod_devicetable.h>
+
+#define	LKPI_PLATFORM_MAX_RES	8
+
+struct lkpi_platform_resource {
+	resource_size_t		start;
+	resource_size_t		end;
+	unsigned long		flags;
+};
 
 struct platform_device {
-	const char			*name;
-	int				id;
-	bool				id_auto;
-	struct device			dev;
+	const char		*name;
+	int			id;
+	bool			id_auto;
+	struct device		dev;
+	struct lkpi_platform_resource lkpi_res[LKPI_PLATFORM_MAX_RES];
+	int			num_resources;
+	uint32_t		node;
+	void			*bsd_res[LKPI_PLATFORM_MAX_RES];
+	int			bsd_rid[LKPI_PLATFORM_MAX_RES];
+	int			bsd_nres;
 };
 
 struct platform_driver {
-	void				(*remove)(struct platform_device *);
-	struct device_driver		driver;
+	int  (*probe)(struct platform_device *);
+	void (*remove)(struct platform_device *);
+	void (*shutdown)(struct platform_device *);
+	struct device_driver	driver;
+	driver_t		bsddriver;
+	devclass_t		bsdclass;
 };
 
-#define	dev_is_platform(dev)	(false)
-#define	to_platform_device(dev)	(NULL)
+#define	to_platform_device(d) \
+	container_of(d, struct platform_device, dev)
+
+#define	platform_get_drvdata(pdev) \
+	dev_get_drvdata(&(pdev)->dev)
+#define	platform_set_drvdata(pdev, data) \
+	dev_set_drvdata(&(pdev)->dev, data)
+
+int linux_platform_register_driver(struct platform_driver *);
+void linux_platform_unregister_driver(struct platform_driver *);
+
+struct lkpi_platform_resource *platform_get_resource(
+    struct platform_device *, unsigned int, unsigned int);
+int platform_get_irq(struct platform_device *, unsigned int);
+void __iomem *devm_platform_ioremap_resource(struct platform_device *,
+    unsigned int);
+
+#define	module_platform_driver(_drv)					\
+	module_driver(_drv, linux_platform_register_driver,		\
+	    linux_platform_unregister_driver)
+
+#define	builtin_platform_driver(_drv) module_platform_driver(_drv)
 
 static __inline int
 platform_driver_register(struct platform_driver *pdrv)
 {
-
-	pr_debug("%s: TODO\n", __func__);
-	return (-ENXIO);
-}
-
-static __inline void *
-dev_get_platdata(struct device *dev)
-{
-
-	pr_debug("%s: TODO\n", __func__);
-	return (NULL);
-}
-
-static __inline int
-platform_driver_probe(struct platform_driver *pdrv,
-    int(*pd_probe_f)(struct platform_device *))
-{
-
-	pr_debug("%s: TODO\n", __func__);
-	return (-ENODEV);
+	return (linux_platform_register_driver(pdrv));
 }
 
 static __inline void
 platform_driver_unregister(struct platform_driver *pdrv)
 {
-
-	pr_debug("%s: TODO\n", __func__);
-	return;
+	linux_platform_unregister_driver(pdrv);
 }
 
 static __inline int
-platform_device_register(struct platform_device *pdev)
+platform_device_register(struct platform_device *pdev __unused)
 {
-	pr_debug("%s: TODO\n", __func__);
 	return (0);
 }
 
 static __inline void
-platform_device_unregister(struct platform_device *pdev)
+platform_device_unregister(struct platform_device *pdev __unused)
 {
+}
 
-	pr_debug("%s: TODO\n", __func__);
-	return;
+static __inline void *
+dev_get_platdata(struct device *dev __unused)
+{
+	return (NULL);
+}
+
+static __inline bool
+dev_is_platform(struct device *dev __unused)
+{
+	return (false);
 }
 
 #endif	/* _LINUXKPI_LINUX_PLATFORM_DEVICE_H */
---
+++ sys/compat/linuxkpi/common/src/linux_platform.c
@@ -0,0 +1,313 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 Tej Kiran
+ *
+ * LinuxKPI platform_device/platform_driver shim.
+ * Bridges Linux platform_driver to FreeBSD newbus + OFW,
+ * following the same pattern as linux_pci.c.
+ */
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/bus.h>
+#include <sys/rman.h>
+
+#ifdef FDT
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+#endif
+
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+#define	LKPI_IORESOURCE_MEM	(1 << SYS_RES_MEMORY)
+#define	LKPI_IORESOURCE_IO	(1 << SYS_RES_IOPORT)
+#define	LKPI_IORESOURCE_IRQ	(1 << SYS_RES_IRQ)
+
+static int linux_platform_probe(device_t dev);
+static int linux_platform_attach(device_t dev);
+static int linux_platform_detach(device_t dev);
+
+static device_method_t platform_methods[] = {
+	DEVMETHOD(device_probe,		linux_platform_probe),
+	DEVMETHOD(device_attach,	linux_platform_attach),
+	DEVMETHOD(device_detach,	linux_platform_detach),
+	DEVMETHOD_END
+};
+
+static struct platform_driver *
+linux_platform_get_driver(device_t dev)
+{
+	driver_t *drv;
+
+	drv = device_get_driver(dev);
+	if (drv == NULL)
+		return (NULL);
+	return (container_of(drv, struct platform_driver, bsddriver));
+}
+
+#ifdef FDT
+static const struct of_device_id *
+linux_platform_match_of(device_t dev, const struct of_device_id *table)
+{
+	const struct of_device_id *id;
+
+	if (table == NULL)
+		return (NULL);
+	for (id = table; id->compatible[0] != '\0'; id++) {
+		if (ofw_bus_is_compatible(dev, id->compatible))
+			return (id);
+	}
+	return (NULL);
+}
+#endif
+
+static int
+linux_platform_probe(device_t dev)
+{
+#ifdef FDT
+	struct platform_driver *pdrv;
+	const struct of_device_id *id;
+
+	pdrv = linux_platform_get_driver(dev);
+	if (pdrv == NULL)
+		return (ENXIO);
+
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	id = linux_platform_match_of(dev, pdrv->driver.of_match_table);
+	if (id == NULL)
+		return (ENXIO);
+
+	device_set_desc(dev, pdrv->driver.name);
+	return (BUS_PROBE_DEFAULT);
+#else
+	return (ENXIO);
+#endif
+}
+
+static int
+linux_platform_attach(device_t dev)
+{
+	struct platform_driver *pdrv;
+	struct platform_device *pdev;
+#ifdef FDT
+	struct device_node *np;
+#endif
+	int error;
+
+	pdrv = linux_platform_get_driver(dev);
+	MPASS(pdrv != NULL);
+
+	pdev = device_get_softc(dev);
+	MPASS(pdev != NULL);
+
+	memset(pdev, 0, sizeof(*pdev));
+	pdev->dev.bsddev = dev;
+	pdev->name = pdrv->driver.name;
+	pdev->id = -1;
+
+	spin_lock_init(&pdev->dev.devres_lock);
+	INIT_LIST_HEAD(&pdev->dev.devres_head);
+	INIT_LIST_HEAD(&pdev->dev.irqents);
+
+	pdev->dev.driver = &pdrv->driver;
+	pdev->dev.parent = &linux_root_device;
+
+#ifdef FDT
+	pdev->node = ofw_bus_get_node(dev);
+
+	np = kmalloc(sizeof(*np), GFP_KERNEL);
+	if (np == NULL)
+		return (ENOMEM);
+	np->phandle = pdev->node;
+	np->full_name = pdrv->driver.name;
+	pdev->dev.of_node = np;
+#endif
+
+	if (pdrv->probe == NULL)
+		return (ENXIO);
+
+	error = pdrv->probe(pdev);
+	if (error != 0) {
+#ifdef FDT
+		kfree(pdev->dev.of_node);
+		pdev->dev.of_node = NULL;
+#endif
+		return (-error);
+	}
+
+	return (0);
+}
+
+static int
+linux_platform_detach(device_t dev)
+{
+	struct platform_driver *pdrv;
+	struct platform_device *pdev;
+	struct resource *bsd_res;
+	int i;
+
+	pdrv = linux_platform_get_driver(dev);
+	pdev = device_get_softc(dev);
+
+	if (pdrv != NULL && pdrv->remove != NULL)
+		pdrv->remove(pdev);
+
+	for (i = 0; i < pdev->bsd_nres; i++) {
+		bsd_res = pdev->bsd_res[i];
+		if (bsd_res != NULL) {
+			bus_release_resource(dev, rman_get_type(bsd_res),
+			    pdev->bsd_rid[i], bsd_res);
+			pdev->bsd_res[i] = NULL;
+		}
+	}
+	pdev->bsd_nres = 0;
+
+#ifdef FDT
+	kfree(pdev->dev.of_node);
+	pdev->dev.of_node = NULL;
+#endif
+
+	return (0);
+}
+
+struct lkpi_platform_resource *
+platform_get_resource(struct platform_device *pdev, unsigned int type,
+    unsigned int index)
+{
+	struct resource *bsd_res;
+	int bsd_type;
+	int rid;
+
+	switch (type) {
+	case LKPI_IORESOURCE_MEM:
+		bsd_type = SYS_RES_MEMORY;
+		break;
+	case LKPI_IORESOURCE_IRQ:
+		bsd_type = SYS_RES_IRQ;
+		break;
+	case LKPI_IORESOURCE_IO:
+		bsd_type = SYS_RES_IOPORT;
+		break;
+	default:
+		return (NULL);
+	}
+
+	rid = index;
+	bsd_res = bus_alloc_resource_any(pdev->dev.bsddev, bsd_type,
+	    &rid, RF_ACTIVE | RF_SHAREABLE);
+	if (bsd_res == NULL)
+		return (NULL);
+
+	if (pdev->bsd_nres >= LKPI_PLATFORM_MAX_RES) {
+		bus_release_resource(pdev->dev.bsddev, bsd_type, rid, bsd_res);
+		return (NULL);
+	}
+
+	pdev->bsd_res[pdev->bsd_nres] = bsd_res;
+	pdev->bsd_rid[pdev->bsd_nres] = rid;
+	pdev->bsd_nres++;
+
+	pdev->lkpi_res[index].start = rman_get_start(bsd_res);
+	pdev->lkpi_res[index].end = rman_get_end(bsd_res);
+	pdev->lkpi_res[index].flags = type;
+	if (pdev->num_resources <= (int)index)
+		pdev->num_resources = index + 1;
+
+	return (&pdev->lkpi_res[index]);
+}
+
+int
+platform_get_irq(struct platform_device *pdev, unsigned int index)
+{
+	struct resource *bsd_res;
+	int rid;
+
+	rid = index;
+	bsd_res = bus_alloc_resource_any(pdev->dev.bsddev, SYS_RES_IRQ,
+	    &rid, RF_ACTIVE | RF_SHAREABLE);
+	if (bsd_res == NULL)
+		return (-ENXIO);
+
+	if (pdev->bsd_nres < LKPI_PLATFORM_MAX_RES) {
+		pdev->bsd_res[pdev->bsd_nres] = bsd_res;
+		pdev->bsd_rid[pdev->bsd_nres] = rid;
+		pdev->bsd_nres++;
+	}
+
+	return (rman_get_start(bsd_res));
+}
+
+void __iomem *
+devm_platform_ioremap_resource(struct platform_device *pdev,
+    unsigned int index)
+{
+	struct resource *bsd_res;
+	int rid;
+
+	rid = index;
+	bsd_res = bus_alloc_resource_any(pdev->dev.bsddev, SYS_RES_MEMORY,
+	    &rid, RF_ACTIVE);
+	if (bsd_res == NULL)
+		return (ERR_PTR(-ENOMEM));
+
+	if (pdev->bsd_nres < LKPI_PLATFORM_MAX_RES) {
+		pdev->bsd_res[pdev->bsd_nres] = bsd_res;
+		pdev->bsd_rid[pdev->bsd_nres] = rid;
+		pdev->bsd_nres++;
+	}
+
+	pdev->lkpi_res[index].start = rman_get_start(bsd_res);
+	pdev->lkpi_res[index].end = rman_get_end(bsd_res);
+	pdev->lkpi_res[index].flags = LKPI_IORESOURCE_MEM;
+	if (pdev->num_resources <= (int)index)
+		pdev->num_resources = index + 1;
+
+	return ((void __iomem *)rman_get_bushandle(bsd_res));
+}
+
+int
+linux_platform_register_driver(struct platform_driver *pdrv)
+{
+#ifdef FDT
+	devclass_t dc;
+
+	if (pdrv->driver.name == NULL)
+		return (-EINVAL);
+
+	pdrv->bsddriver.name = pdrv->driver.name;
+	pdrv->bsddriver.methods = platform_methods;
+	pdrv->bsddriver.size = sizeof(struct platform_device);
+
+	dc = devclass_find("simplebus");
+	if (dc == NULL)
+		return (-ENOENT);
+
+	return (-devclass_add_driver(dc, &pdrv->bsddriver,
+	    BUS_PASS_DEFAULT, &pdrv->bsdclass));
+#else
+	return (-ENOENT);
+#endif
+}
+
+void
+linux_platform_unregister_driver(struct platform_driver *pdrv)
+{
+#ifdef FDT
+	devclass_t dc;
+
+	dc = devclass_find("simplebus");
+	if (dc == NULL)
+		return;
+	devclass_delete_driver(dc, &pdrv->bsddriver);
+#endif
+}
