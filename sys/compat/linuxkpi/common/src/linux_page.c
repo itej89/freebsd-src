7cbe1c544ed5d54312093f7936407f44efcbc6cb
Tej Kiran <kiran.tej@amd.com>
2026-08-08 08:33:46 -0500

    linuxkpi: Remove vmap debug print

--- sys/compat/linuxkpi/common/src/linux_page.c
+++ sys/compat/linuxkpi/common/src/linux_page.c
@@ -458,8 +458,6 @@ vmap(struct page **pages, unsigned int count, unsigned long flags, int prot)
 					va += PAGE_SIZE;
 				}
 				sfence_vma();
-				printf("vmap: uncached mapping %u pages at VA=%p (uc_off=0x%lx)\n",
-				    count, (void *)off, (unsigned long)uc_off);
 				return ((void *)off);
 			}
 		}
