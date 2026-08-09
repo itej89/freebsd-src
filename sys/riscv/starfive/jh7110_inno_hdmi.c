50ed17365f9aeb2ea35e56de8b76206107a3a315
Tej Kiran <kiran.tej@amd.com>
2026-08-08 23:25:35 -0500

    jh7110_inno_hdmi: Debug DC8200 node lookup path

--- sys/riscv/starfive/jh7110_inno_hdmi.c
+++ sys/riscv/starfive/jh7110_inno_hdmi.c
@@ -252,14 +252,27 @@ jh7110_inno_hdmi_attach(device_t dev)
 	{
 		phandle_t dc_node;
 		clk_t dc_clk;
+		hwreset_t dc_rst;
 		int j;
 
 		dc_node = OF_finddevice("/soc/dc8200@29400000");
+		device_printf(dev, "DC8200 node lookup: %d\n", (int)dc_node);
+		if (dc_node <= 0) {
+			phandle_t root = OF_finddevice("/");
+			phandle_t soc = OF_finddevice("/soc");
+			device_printf(dev, "root=%d soc=%d\n",
+			    (int)root, (int)soc);
+		}
 		if (dc_node > 0) {
 			for (j = 0; clk_get_by_ofw_index(dev, dc_node, j,
 			    &dc_clk) == 0; j++)
 				clk_enable(dc_clk);
 			device_printf(dev, "enabled %d DC8200 clocks\n", j);
+
+			for (j = 0; hwreset_get_by_ofw_idx(dev, dc_node, j,
+			    &dc_rst) == 0; j++)
+				hwreset_deassert(dc_rst);
+			device_printf(dev, "deasserted %d DC8200 resets\n", j);
 		}
 	}
 
@@ -267,21 +280,6 @@ jh7110_inno_hdmi_attach(device_t dev)
 	if (hwreset_get_by_ofw_idx(dev, 0, 0, &rst) == 0) {
 		hdmi_rst = rst;
 		hwreset_deassert(rst);
-	}
-
-	/* Deassert DC8200 resets */
-	{
-		phandle_t dc_node;
-		hwreset_t dc_rst;
-		int j;
-
-		dc_node = OF_finddevice("/soc/dc8200@29400000");
-		if (dc_node > 0) {
-			for (j = 0; hwreset_get_by_ofw_idx(dev, dc_node, j,
-			    &dc_rst) == 0; j++)
-				hwreset_deassert(dc_rst);
-			device_printf(dev, "deasserted %d DC8200 resets\n", j);
-		}
 	}
 
 	DELAY(50000);
