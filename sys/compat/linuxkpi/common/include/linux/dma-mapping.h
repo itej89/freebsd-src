f9ab85bb0211e6601094bd9bc7f9a8dca24ea276
Tej Kiran <kiran.tej@amd.com>
2026-08-09 20:46:01 -0500

    linuxkpi: Revert to uncached SG pager for dma_mmap_wc

--- sys/compat/linuxkpi/common/include/linux/dma-mapping.h
+++ sys/compat/linuxkpi/common/include/linux/dma-mapping.h
@@ -474,10 +474,10 @@ static inline int
 dma_mmap_wc(struct device *dev, struct vm_area_struct *vma,
     void *cpu_addr, dma_addr_t dma_addr, size_t size)
 {
-	/*
-	 * Map cached for fast CPU writes. The DRM driver flushes
-	 * the L2 cache before scanout via sifive_ccache_flush_all().
-	 */
+	vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
+	vma->vm_pfn = dma_addr >> PAGE_SHIFT;
+	vma->vm_len = size;
+	vma->vm_ops = NULL;
 	return (0);
 }
 
